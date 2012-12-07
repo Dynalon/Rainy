e23b9b4f955384225f2a63808ecb762dd3ecf0ed
Timo Dörr <timo@latecrew.de>
2012-12-07 09:55:06 +0100

    Added unit test for OAuth token exchange

--- Rainy-tests/BasicConnection.cs
+++ Rainy-tests/BasicConnection.cs
@@ -3,6 +3,11 @@ using Rainy;
 using NUnit.Framework;
 using ServiceStack.ServiceClient.Web;
 using Rainy.WebService;
+using DevDefined.OAuth.Consumer;
+using System.Security.Cryptography;
+using DevDefined.OAuth.Framework;
+using System.Net;
+using System.Linq;
 
 namespace Rainy.Tests
 {
@@ -44,6 +49,44 @@ namespace Rainy.Tests
 			var restClient = new JsonServiceClient (baseUri);
 
 			restClient.Get<UserResponse> (apiResponse.UserRef.ApiRef);
+
+		}
+
+		[Test()]
+		public void OAuthGetRequestToken ()
+		{
+			var resp = GetRootApiRef ();
+			var consumerContext = new OAuthConsumerContext (){
+				ConsumerKey = "anyone"
+			};
+
+			var session = new OAuthSession (consumerContext, resp.OAuthRequestTokenUrl,
+			                                resp.OAuthAuthorizeUrl, resp.OAuthAccessTokenUrl);
+
+			IToken request_token = session.GetRequestToken ();
+
+			string link = session.GetUserAuthorizationUrlForToken (request_token, "http://example.com/");
+
+			// visit the link to perform the authrization (no interaction needed)
+			HttpWebRequest req = (HttpWebRequest) HttpWebRequest.Create (link);
+			// disallow auto redirection, since we are interested in the location header only
+			req.AllowAutoRedirect = false;
+
+			string location = ((HttpWebResponse) req.GetResponse ()).Headers["Location"];
+			Assert.That (!string.IsNullOrEmpty (location));
+
+			var query = string.Join ("", location.Split ('?').Skip(1));
+			var oauth_data = System.Web.HttpUtility.ParseQueryString (query);
+
+			Assert.AreEqual (request_token.Token, oauth_data["oauth_token"]);
+			Assert.That (!string.IsNullOrEmpty (oauth_data["oauth_verifier"]));
+			Assert.That (oauth_data["oauth_verifier"].Length > 12);
+
+			IToken accessToken = session.ExchangeRequestTokenForAccessToken(request_token, oauth_data["oauth_verifier"]);
+
+			Assert.AreNotEqual (accessToken.Token, request_token.Token);
+			Assert.AreNotEqual (accessToken.TokenSecret, request_token.TokenSecret);
+
 
 		}
 	}
