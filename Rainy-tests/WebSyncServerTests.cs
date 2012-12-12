04b27d798987131bb45b52cea4a09a7d957af06c
Timo Dörr <timo@latecrew.de>
2012-12-12 10:18:15 +0100

    Added delete notes unit tests for WebSyncServer

--- Rainy-tests/WebSyncServerTests.cs
+++ Rainy-tests/WebSyncServerTests.cs
@@ -20,6 +20,8 @@ namespace Rainy.Tests
 		{
 			var server = new WebSyncServer (baseUri, GetAccessToken ());
 			server.BeginSyncTransaction ();
+
+			Assert.That (!string.IsNullOrEmpty (server.Id));
 		}
 		
 		[Test()]
@@ -33,9 +35,13 @@ namespace Rainy.Tests
 			// after upload, we should be able to get that very same notes
 			var received_notes = server.GetAllNotes (true);
 
-			Assert.AreEqual(sampleNotes.Count, received_notes.Count);
+			Assert.AreEqual (sampleNotes.Count, received_notes.Count);
+			Assert.AreEqual (sampleNotes.Count, server.UploadedNotes.Count);
+
+
 			sampleNotes.ToList().ForEach (local_note => {
 
+				Assert.That (server.UploadedNotes.Contains (local_note));
 				// pick the note from out returned notes list as the order may be
 				// different
 				var server_note = received_notes.Where (n => n.Guid == local_note.Guid).FirstOrDefault ();
@@ -50,6 +56,7 @@ namespace Rainy.Tests
 				Assert.AreEqual(local_note.MetadataChangeDate, server_note.MetadataChangeDate);
 				Assert.AreEqual(local_note.ChangeDate, server_note.ChangeDate);
 
+
 			});
 
 		}
@@ -60,6 +67,43 @@ namespace Rainy.Tests
 			var server = new WebSyncServer (baseUri, GetAccessToken ());
 			server.BeginSyncTransaction ();
 			server.GetAllNotes (true);
+		}
+
+		[Test()]
+		public void WebSyncServerDeleteAllNotes()
+		{
+			var server = new WebSyncServer (baseUri, GetAccessToken ());
+			server.BeginSyncTransaction ();
+
+			server.UploadNotes (sampleNotes);
+			server.DeleteNotes (sampleNotes.Select (n => n.Guid).ToList ());
+			var server_notes = server.GetAllNotes (false);
+
+			Assert.AreEqual (0, server_notes.Count);
+
+			server.DeletedServerNotes.ToList ().ForEach (deleted_note_guid => {
+				Assert.That (sampleNotes.Select(n => n.Guid).Contains (deleted_note_guid));
+			});
+		}
+		[Test()]
+		public void WebSyncServerDeleteSingleNote ()
+		{
+			var server = new WebSyncServer (baseUri, GetAccessToken ());
+			server.BeginSyncTransaction ();
+
+			server.UploadNotes (sampleNotes);
+			var deleted_note = sampleNotes.First ();
+			server.DeleteNotes (new List<string> () { deleted_note.Guid });
+
+			var server_notes = server.GetAllNotes (false);
+
+			// 2 notes should remain on the server
+			Assert.AreEqual (2, server_notes.Count);
+			// the deleted note should not be one of them
+			Assert.AreEqual (0, server_notes.Where (n => n.Guid == deleted_note.Guid).Count ());
+
+			Assert.AreEqual (deleted_note.Guid, server.DeletedServerNotes.First ());
+
 		}
 	}
 }
