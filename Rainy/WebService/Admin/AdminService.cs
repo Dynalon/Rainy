3a6a5955c9ec8c234e73ff0cb0f8164ef8576bbd
Timo Dörr <timo@latecrew.de>
2013-02-04 21:17:35 +0100

    Update admin service

--- Rainy/WebService/Admin/AdminService.cs
+++ Rainy/WebService/Admin/AdminService.cs
@@ -1,23 +1,18 @@
 using System;
 using Rainy.Db;
-using ServiceStack.ServiceInterface;
 using ServiceStack.ServiceHost;
 using ServiceStack.OrmLite;
 using ServiceStack.Common;
-using ServiceStack.Text;
+using ServiceStack.Common.Web;
+using System.Net;
 
 namespace Rainy.WebService.Admin
 {
 	// TODO only a logged in admin
 	// should be able to access
-	[Route("/admin/user/{Username}","GET")]
-	public class AdminUserRequest : IReturn<DBUser>
-	{
-		public string Username { get; set; }
-	}
-
 	[Route("/admin/user/","POST,PUT")]
-	public class AdminAddOrUpdateUserRequest : DBUser, IReturn<DBUser>
+	[Route("/admin/user/{Username}","GET,DELETE")]
+	public class AdminUserRequest : DBUser, IReturn<DBUser>
 	{
 	}
 
@@ -26,51 +21,128 @@ namespace Rainy.WebService.Admin
 		public AdminUserService () : base ()
 		{
 		}
-		public object Get (AdminUserRequest req)
+		public DBUser Get (AdminUserRequest req)
 		{
-			DBUser user;
+			DBUser found_user;
+
 			using (var conn = DbConfig.GetConnection ()) {
-				user = conn.FirstOrDefault<DBUser> ("Username = {0}", req.Username);
+				found_user = conn.FirstOrDefault<DBUser> ("Username = {0}", req.Username);
 			}
-			if (user == null) throw new Exception ("User not found!");
-			return user;
+			if (found_user == null) throw new Exception ("User not found!");
+			return found_user;
 		}
 
-		public object Post (AdminAddOrUpdateUserRequest user)
+		// TODO see if we can directly use DBUser
+		// update existing user
+		public object Put (AdminUserRequest updated_user)
+		{
+			var user = new DBUser ();
+			updated_user.Manifest = null;
+			// TODO don't touch the SyncManifest when updating the user
+			user.PopulateWith (updated_user);
+
+			using (var conn = DbConfig.GetConnection ()) {
+				var stored_user = conn.FirstOrDefault<DBUser>("Username = {0}", user.Username);
+
+				if (stored_user == null) {
+					// user did not exist, can't update
+					return new HttpResult {
+						Status = 404,
+						StatusDescription = "User " + user.Username + " was not found," +
+							" and can't be updated. Try using HTTP POST to create a new user"
+					};
+				}
+
+				if (user.Password == "") {
+					// password was not sent so use the old password
+					// TODO hashing
+					user.Password = stored_user.Password;
+				}
+
+				conn.Update<DBUser> (user, u => u.Username == user.Username);
+			}
+			Logger.DebugFormat ("updating user information for user {0}", user.Username);
+
+			// do not return the password over the wire
+			user.Password = "";
+			return new HttpResult (user) {
+				StatusCode = System.Net.HttpStatusCode.OK,
+				StatusDescription = "Successfully updated user " + user.Username
+			};
+		}
+
+		/// <summary>
+		/// POST /admin/user
+		/// 
+		/// creates a new user.
+		/// 
+		/// returns HTTP Response =>
+		/// 	201 Created
+		/// 	Location: http://localhost/admin/user/{Username}
+		/// </summary>	
+		public object Post (AdminUserRequest user)
 		{
 			var new_user = new DBUser ();
 			new_user.PopulateWith (user);
-			user.Password ="blubb";
-			user.EmailAddress = "bla@blubb.com";
-			user.PrintDump ();
 
 			using (var conn = DbConfig.GetConnection ()) {
-				using (var trans = conn.BeginTransaction ()) {
-					var stored_user = conn.FirstOrDefault<DBUser>("Username = {0}", new_user.Username);
-
-					if (stored_user != null && new_user.Password == "") {
-						// user exists, but password was not updated so use the old password
-						// TODO hashing
-						new_user.Password = stored_user.Password;
-					} else {
-						// we can delete the user as we have all data for a reinsert
-						conn.Delete<DBUser> ("Username = {0}", new_user.Username);
-					}
+				try {
+					var existing_user = conn.FirstOrDefault<DBUser> ("Username = {0}", new_user.Username);
+					if (existing_user != null)
+						throw new Exception ("A user by that name already exists");
 
 					conn.Insert<DBUser> (new_user);
-					trans.Commit ();
+				} catch (Exception e) {
+					Logger.DebugFormat ("error inserting user {0} into the database, msg was {1}",
+					                    new_user.Username, e.Message);
+					return new HttpResult {
+						StatusCode = HttpStatusCode.Conflict,
+						StatusDescription = "Conflict! " + e.Message
+					};
 				}
 			}
-			Logger.DebugFormat ("inserting/updating user information for user {0}", new_user.Username);
-			new_user.PrintDump ();
-			// do not return the password over the wire
-			new_user.Password = "";
-			return new_user;
+
+
+			return new HttpResult (new_user) {
+				StatusCode = HttpStatusCode.Created,
+				StatusDescription = "Sucessfully created user " + new_user.Username,
+				Headers = {
+					{ HttpHeaders.Location, base.Request.AbsoluteUri.CombineWith (new_user.Username) }
+				}
+			};
 		}
 
-		public object Put (AdminAddOrUpdateUserRequest user)
+		/// <summary>
+		/// DELETE /admin/user/{Username}
+		/// 
+		/// creates a new user.
+		/// 
+		/// returns HTTP Response =>
+		/// 	204 No Content
+		/// 	Location: http://localhost/admin/user/{Username}
+		/// </summary>
+		public object Delete (AdminUserRequest user)
 		{
-			return Post (user);
+			using (var conn = DbConfig.GetConnection ()) {
+				try {
+					conn.Delete<DBUser>(u => u.Username == user.Username);
+				} catch (Exception e) {
+					Logger.DebugFormat ("error deleting user {0}, msg was: {1}",
+					                    user.Username, e.Message);
+
+					return new HttpResult {
+						StatusCode = HttpStatusCode.InternalServerError,
+						StatusDescription = "Error occured, msg was: " + e.Message
+					};
+				}
+			}
+
+			return new HttpResult {
+				StatusCode = HttpStatusCode.NoContent,
+				Headers = {
+					{ HttpHeaders.Location, this.RequestContext.AbsoluteUri }
+				}
+			};
 		}
 	}
 }
