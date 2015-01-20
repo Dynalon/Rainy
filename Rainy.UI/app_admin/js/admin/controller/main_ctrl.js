d46b8f623ee3199a05f0f5be23d39720f706bcd2
Timo Dörr <timo@latecrew.de>
2015-01-20 21:42:19 +0100

    Remove hack that was required due to a bug in Angular < 1.2

--- Rainy.UI/app_admin/js/admin/controller/main_ctrl.js
+++ Rainy.UI/app_admin/js/admin/controller/main_ctrl.js
@@ -11,12 +11,6 @@ function MainCtrl($scope, $routeParams, $route, $location, adminPassword) {
     };
     $scope.adminPassword = adminPassword;
     $scope.checkLocation();
-
-    // bug in angular prevents this from firing when the back button is used
-    // (fixed in 1.1.5) - see https://github.com/angular/angular.js/pull/2206
-    $scope.$on('$locationChangeStart', function(ev, oldloc, newloc) {
-        //$scope.checkLocation();
-    });
 }
 
 MainCtrl.$inject = [ '$scope', '$routeParams', '$route', '$location' ];
